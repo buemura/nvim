2e25a2c1a7f056013b81c216ae54742b1a6ac762
buemura <bruno.uemura1@gmail.com>
2024-02-07 14:16:05 -0300

    feat: adds lua lsp

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    confg = function()
+      requie("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+    end
+  }
+}
