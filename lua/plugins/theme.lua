1c9e69daec7edef2b2b8c69eea7513d283df4684
buemura <bruno.uemura1@gmail.com>
2024-02-07 23:13:15 -0300

    feat: changes theme to tokyo night

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,21 @@
+return {
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = false,
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("catppuccin")
+  --   end,
+  -- },
+  {
+   "folke/tokyonight.nvim",
+   lazy = false,
+   name = "tokyonight",
+   priority = 1000,
+   config = function()
+     vim.cmd("colorscheme tokyonight-night")
+   end
+  }
+}
+
