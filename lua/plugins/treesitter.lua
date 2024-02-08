d7f0ce97b37266cbe713bca8b4e7e02ab54779b6
buemura <bruno.uemura1@gmail.com>
2024-02-07 22:19:21 -0300

    feat: adds none-ls for linting and formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,9 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "html"},
+      auto_install = true,
       highlight = { enable = true },
-      indent = { enable = true }, 
+      indent = { enable = true },
     })
-  end
+  end,
 }
